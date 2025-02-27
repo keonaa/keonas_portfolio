0ac3885d7571c7aa51d784cb376ef24958574802
keonaa <keona.aguilar@gmail.com>
2025-02-26 19:16:54 -0800

    fixed hero

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,7 +3,7 @@ import styles from "./hero.module.css";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Hey! I'm Keona</h1>
+      <h1>{`Hey! I'm Keona`}</h1>
       <p style={{ textTransform: "uppercase" }}>A multidisciplinary digital designer specializing in UX/UI design.</p>
     </section>
   );
