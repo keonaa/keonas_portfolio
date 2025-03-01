da7c48d5e0981cc73cd123b7fd4b40b8198808a1
keonaa <keona.aguilar@gmail.com>
2025-02-28 18:28:06 -0800

    worked on landing page

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,10 +1,29 @@
 import styles from "./hero.module.css";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>{`Hey! I'm Keona`}</h1>
-      <p style={{ textTransform: "uppercase" }}>A multidisciplinary digital designer specializing in UX/UI design.</p>
+      <h1>Hey! I'm Keona</h1>
+      <p>A MULTIDISCIPLINARY DIGITAL DESIGNER, SPECIALIZING IN UX/UI DESIGN.</p>
+      
+      <div className={styles.chipsContainer}>
+        <span className={styles.chip}>UX/UI DESIGNER</span>
+        <span className={styles.chip}>GRAPHIC DESIGN</span>
+        <span className={styles.chip}>VIDEO EDITOR</span>
+        <span className={styles.chip}>STORYTELLER</span>
+      </div>
+
+      <div className={styles.scrollContainer}>
+        <p className={styles.scrollText}>SCROLL</p>
+        <Image 
+          src="/scroll_arrow.svg"
+          alt="Scroll Arrow"
+          width={30}
+          height={30} 
+          className={styles.scrollArrow} 
+        />
+      </div>
     </section>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,30 @@
 import Hero from "./components/Hero";
+import styles from "./page.module.css";
 
 
 export default function Home() {
   return (
     <main>
       <Hero />
- 
+      <section className={styles.purposeSection}>
+        <div className={styles.purposeContent}>
+          <h3 className={styles.purposeTitle}>Purpose. Connection. Impact.</h3>
+          <div className={styles.purposeText}>
+            <p>
+              DRIVEN BY THE BELIEF THAT EACH DESIGN HAS ITS OWN UNIQUE STORY THAT CAN CREATE AN IMPACT, 
+              I IDENTIFY CREATIVE OPPORTUNITIES TO DELIVER DESIGN SOLUTIONS THAT CONNECT PEOPLE.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.featuredProjects}>
+        <div className={styles.featuredProjectsContent}>
+          <h2 className={styles.featuredProjectsTitle}>Featured Projects</h2>
+          <div className={styles.cards}>
+
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
