b0788166394a668a74e07955ef99069eb4dcb5b7
keonaa <keona.aguilar@gmail.com>
2025-03-02 21:51:04 -0800

    added fizzle card to projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,11 @@
 import styles from "./project.module.css";
+import FizzleCard from "../components/FizzleCard";
 
 export default function Projects() {
     return(
         <section className={styles.projects}>
             <h2>Projects</h2>
+            <FizzleCard />
         </section>
     );
 }
